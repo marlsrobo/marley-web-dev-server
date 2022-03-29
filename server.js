473dbf47dd71b280396af1599bf5b3c71a2b60a5
Marley Robinson <marlsrobo@gmail.com>
2022-03-29 17:54:31 -0400

    exercise 5 done

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/users/user-controller.js";
+import tuitsController from "./controllers/tuits/tuits-controller.js";
+
 const app = express();
 app.use(express.json());
 helloController(app);
 userController(app);
+tuitsController(app);
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')});
 app.listen(4000);
