dc298d8345fb6ea555dea3ca6019183268d8d9aa
Marley Robinson <marlsrobo@gmail.com>
2022-04-04 14:37:20 -0400

    a9 done?

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,37 +1,27 @@
-import posts from "./tuits.js";
-let tuits = posts;
+import tuitsDao from "../../tuits-dao.js";
 
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
     const newTuit = req.body;
-    newTuit._id = (new Date()).getTime() + '';
-    newTuit.likes = 0;
-    newTuit.dislikes = 0;
-    newTuit.postedBy = {username: "Marley Robinson"};
-    newTuit.stats = {comments: 0,
-                     retuits: 0,
-                     likes: 0};
-    newTuit.handle = "mrobinson";
-    newTuit.title = '';
-    newTuit.time = "just now";
-    newTuit.attachments = {img: "https://artsy-media-uploads.s3.amazonaws.com/2P6t_Yt6dF0TNN76dlp-_Q%2F3417757448_4a6bdf36ce_o.jpg"};
-    newTuit.avatarImg = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-    tuits.unshift(newTuit);
-    res.json(newTuit);
+    const insertedTuit = await tuitsDao.createTuit(newTuit);
+    res.json(insertedTuit);
 };
 
-const findAllTuits = (req, res) => res.json(tuits);
+const findAllTuits = async (req, res) => {
+    const tuits = await tuitsDao.findAllTuits();
+    res.json(tuits);
+};
 
-const updateTuit = (req, res) => {
+const updateTuit = async (req, res) => {
     const tuitIdToUpdate = req.params.tid;
     const updatedTuit = req.body;
-    tuits = tuits.map(t => t._id === tuitIdToUpdate ? updatedTuit : t);
-    res.sendStatus(200);
+    const status = await tuitsDao.updateTuit(tuitIdToUpdate, updatedTuit);
+    res.send(status);
 };
 
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
     const tuitIdToDelete = req.params.tid;
-    tuits = tuits.filter(t => t._id !== tuitIdToDelete);
-    res.sendStatus(200);
+    const status = await tuitsDao.deleteTuit(tuitIdToDelete);
+    res.send(status);
 };
 
 export default (app) => {
